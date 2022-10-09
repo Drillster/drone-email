787a92dd6a72770915d6fcc959e97b0c541b8592
Jonas Bernard <contact@jonas-bernard.dev>
2022-10-09 13:46:13 +0200

    Remove unused html and CSS

--- defaults.go
+++ defaults.go
@@ -23,7 +23,22 @@ const DefaultTemplate = `
 <head>
     <meta name="viewport" content="width=device-width" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="color-scheme" content="dark light">
     <style>
+        * {
+          font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+          box-sizing: border-box;
+          font-size: 14px;
+        }
+
+        body {
+          -webkit-font-smoothing: antialiased;
+          -webkit-text-size-adjust: none;
+          width: 100% !important;
+          height: 100%;
+          line-height: 1.6;
+        }
+
         @media (prefers-color-scheme: light) {
             body {
                 background-color: rgb(30, 30, 30);
@@ -103,11 +118,11 @@ const DefaultTemplate = `
         }
 
         .alert.alert-bad {
-          background: #d0021b;
+          background-color: #d0021b;
         }
 
         .alert.alert-good {
-          background: #68b90f;
+          background-color: #68b90f;
         }
 
         td {
@@ -118,7 +133,7 @@ const DefaultTemplate = `
 <body>
     <p>
       This is the report of your most recent drone pipeline build.
-      It finished {{ datetime build.finished "Mon Jan 2 15:04:05 MST 2006" "Local" }}
+      It finished {{ datetime build.finished "Mon Jan 2 15:04:05 MST 2006" "Local" }}.
     </p>
    
     <!--<img src="https://github.com/JonasBernard/drone-email/raw/master/img/{{build.status}}.png" />-->
@@ -152,15 +167,10 @@ const DefaultTemplate = `
         </div>
     </p>
     
-    <ul>
-        <li>See the build on drone: <a href="{{build.link}}">Build #{{build.number}}, {{build.status}}</a></li>
-        <li>See the commit on GitHub: <a href="{{commit.link}}">{{commit.message}}</a></li>
-        <li>See the repository on GitHub: <a href="{{repo.link}}">{{repo.fullName}}</a></li>
-    </ul>
     <table width="100%" cellpadding="0" cellspacing="0">
     <tr>
       <td>
-	See the build on drone::
+	      See the build on drone:
       </td>
       <td>
 	<a href="{{build.link}}">Build #{{build.number}}, {{build.status}}</a>
@@ -184,14 +194,6 @@ const DefaultTemplate = `
     </tr>
     <tr>
       <td>
-	Commit:
-      </td>
-      <td>
-	{{ truncate commit.sha 8 }}
-      </td>
-    </tr>
-    <tr>
-      <td>
 	Started at:
       </td>
       <td>
@@ -200,168 +202,5 @@ const DefaultTemplate = `
     </tr>
   </table>
 </body>
-
-  <head>
-    
-    <style>
-      * {
-        margin: 0;
-        padding: 0;
-        font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
-        box-sizing: border-box;
-        font-size: 14px;
-      }
-      body {
-        -webkit-font-smoothing: antialiased;
-        -webkit-text-size-adjust: none;
-        width: 100% !important;
-        height: 100%;
-        line-height: 1.6;
-        background-color: #f6f6f6;
-      }
-      table td {
-        vertical-align: top;
-      }
-      .body-wrap {
-        background-color: #f6f6f6;
-        width: 100%;
-      }
-      .container {
-        display: block !important;
-        max-width: 600px !important;
-        margin: 0 auto !important;
-        /* makes it centered */
-        clear: both !important;
-      }
-      .content {
-        max-width: 600px;
-        margin: 0 auto;
-        display: block;
-        padding: 20px;
-      }
-      .main {
-        background: #fff;
-        border: 1px solid #e9e9e9;
-        border-radius: 3px;
-      }
-      .content-wrap {
-        padding: 20px;
-      }
-      .content-block {
-        padding: 0 0 20px;
-      }
-      .header {
-        width: 100%;
-        margin-bottom: 20px;
-      }
-      h1, h2, h3 {
-        font-family: "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
-        color: #000;
-        margin: 40px 0 0;
-        line-height: 1.2;
-        font-weight: 400;
-      }
-      h1 {
-        font-size: 32px;
-        font-weight: 500;
-      }
-      h2 {
-        font-size: 24px;
-      }
-      h3 {
-        font-size: 18px;
-      }
-      hr {
-        border: 1px solid #e9e9e9;
-        margin: 20px 0;
-        height: 1px;
-        padding: 0;
-      }
-      p,
-      ul,
-      ol {
-        margin-bottom: 10px;
-        font-weight: normal;
-      }
-      p li,
-      ul li,
-      ol li {
-        margin-left: 5px;
-        list-style-position: inside;
-      }
-      a {
-        color: #348eda;
-        text-decoration: underline;
-      }
-      .last {
-        margin-bottom: 0;
-      }
-      .first {
-        margin-top: 0;
-      }
-      .padding {
-        padding: 10px 0;
-      }
-      .aligncenter {
-        text-align: center;
-      }
-      .alignright {
-        text-align: right;
-      }
-      .alignleft {
-        text-align: left;
-      }
-      .clear {
-        clear: both;
-      }
-      .alert {
-        font-size: 16px;
-        color: #fff;
-        font-weight: 500;
-        padding: 20px;
-        text-align: center;
-        border-radius: 3px 3px 0 0;
-      }
-      .alert a {
-        color: #fff;
-        text-decoration: none;
-        font-weight: 500;
-        font-size: 16px;
-      }
-      .alert.alert-warning {
-        background: #ff9f00;
-      }
-      .alert.alert-bad {
-        background: #d0021b;
-      }
-      .alert.alert-good {
-        background: #68b90f;
-      }
-      @media only screen and (max-width: 640px) {
-        h1,
-        h2,
-        h3 {
-          font-weight: 600 !important;
-          margin: 20px 0 5px !important;
-        }
-        h1 {
-          font-size: 22px !important;
-        }
-        h2 {
-          font-size: 18px !important;
-        }
-        h3 {
-          font-size: 16px !important;
-        }
-        .container {
-          width: 100% !important;
-        }
-        .content,
-        .content-wrapper {
-          padding: 10px !important;
-        }
-      }
-    </style>
-  </head>
 </html>
 `
